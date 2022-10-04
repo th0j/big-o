fa3b02a00e2a06a03ac4b8f8f804c908a25c8daf
tho.vo <votantho93@gmail.com>
2022-10-04 20:24:25 +0700

    Add array solutions

---
+++ Lecture-01-dynamic-array-and-string/array.py
@@ -0,0 +1,8 @@
+na, nb = list(map(int, input().split()))
+k, m = list(map(int, input().split()))
+
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+
+if a[k-1] < b[nb - m]: print('YES')
+else: print('NO')
